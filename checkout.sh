08169eeb90d2eaddd47022cbfb1d8eea4817fc6c
HongshuW <hongshu.wang.uni@gmail.com>
2024-08-26 20:00:44 +0800

    download all projects

--- checkout.sh
+++ checkout.sh
@@ -4,17 +4,23 @@ export JAVA_HOME=`/usr/libexec/java_home -v 1.8.0_202`
 #!/usr/bin/env bash
 #This script is used to check out all the bug and fix version of the whole repository,
 
+projects=(Chart Cli Closure Codec Collections Compress Csv Gson JacksonCore JacksonDatabind JacksonXml Jsoup JxPath Lang Math Mockito Time)
+bugNums=(26 40 176 18 28 47 16 18 26 112 6 93 22 65 106 38 27)
+
 basePath=/Users/hongshuwang/doc/bug_repo
 
 bid="b"
 fid="f"
-projectName="Lang"
-bugNum="52"
-#projectPath=""
 
-for ((i=0;i<1;i++)); do
+for (( i=0; i<17; i=i+1 ));
+do
+	#echo ${projects[$i]}
+	projectName=${projects[$i]}
+	bugNum=${bugNums[$i]}
 	projectPath="$basePath/$projectName"
-	v=$bugNum
+	for (( j=0; j<bugNum; j=j+1 ));
+	do
+		v=$(($j+1))
 		echo $v
 
 		bugPath="$projectPath/$v"
@@ -36,6 +42,6 @@ for ((i=0;i<1;i++)); do
 		cd $fixPath
 		defects4j compile
 		defects4j test
-#	done
-	
+	done
+
 done
